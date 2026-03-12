e1fb4087edf8181d4f779037e248d4835f7f79d9
vikram soni <jays8174@gmail.com>
2026-03-12 13:02:04 +0530

    get apis

--- src/app.js
+++ src/app.js
@@ -4,19 +4,42 @@ const app = express();
 import User from "./models/user.js";
 
 app.use(express.json());
-app.post("/signup", async(req, res) => {
-const user = new User(req.body);
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
   console.log(req.body);
 
   try {
-     await user.save();
-  res.send("user added successfully");
+    await user.save();
+    res.send("user added successfully");
   } catch (error) {
-    res.statusCode(501).send("error saving user" , + error.message);
+    res.statusCode(501).send("error saving user", +error.message);
   }
- 
- });
+});
+//get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
 
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (user.length === 0) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(user);
+    }
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+//get all user
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+});
 const port = 7777;
 connectDb()
   .then(() => {
