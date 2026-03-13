928bc52ac15dd5a89010f09a0c934f375cca186c
vikram soni <jays8174@gmail.com>
2026-03-13 16:25:32 +0530

    api level senitiation

--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,54 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     firstName: {
-        type: String
+      type: String,
+      required: true,
     },
     lastName: {
-        type: String
+      type: String,
     },
     emailId: {
-        type: String
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
     password: {
-        type: String
+      type: String,
+      required: true,
     },
     age: {
-        type: Number
+      type: Number,
+      required: true,
+      min: 18,
     },
     gender: {
-        type: String
-    }
-});
+      type: String,
+      required: true,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: "",
+    },
+    about: {
+      type: String,
+      default: "this is the default of the user",
+    },
+    skills: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-export default mongoose.model("User",userSchema);
+export default mongoose.model("User", userSchema);
